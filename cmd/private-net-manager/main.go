a4e47746515847a4515a73b7dd93e670f23bba59
刘林欣 <793003251@qq.com>
2019-10-07 22:43:01 +0800

    fix private net manage

--- cmd/private-net-manager/main.go
+++ cmd/private-net-manager/main.go
@@ -12,7 +12,22 @@ import (
 	"path/filepath"
 	"github.com/dipperin/dipperin-core/common/util"
 	"github.com/dipperin/dipperin-core/third-party/crypto"
+	"net"
+	"strconv"
+	"github.com/dipperin/dipperin-core/third-party/p2p/enode"
+	"io/ioutil"
+	"path"
 )
+
+type genesisCfgFile struct {
+	Nonce uint64 `json:"nonce"`
+	//Note       string           `json:"note"`
+	Accounts   map[string]int64 `json:"accounts"`
+	Timestamp  string           `json:"timestamp"`
+	Difficulty string           `json:"difficulty" gencodec:"required"`
+	Verifiers  []string         `json:"verifiers" gencodec:"required"`
+	// todo add a foundation configuration
+}
 
 func main() {
 	app := cli.NewApp()
@@ -52,6 +67,9 @@ func main() {
 		var vDataDirs []string
 		for i := 0; i < 4; i++ {
 			vDataDir := filepath.Join(wDir, fmt.Sprintf("v%v", i))
+			if err := os.MkdirAll(vDataDir, 0755); err != nil {
+				panic(err)
+			}
 			vDataDirs = append(vDataDirs, vDataDir)
 			// 初始化验证者钱包
 			// load wallet manager
@@ -86,18 +104,75 @@ func main() {
 			}
 		}
 
+		// 写入 genesis.json
+		ge := genesisCfgFile{
+			Nonce: 11,
+			Accounts: map[string]int64{},
+			Timestamp: "1548554091989871000",
+			Difficulty: "0x1e566611",
+			Verifiers: vAddresses,
+		}
+		// 创建文件夹
+		if err := os.MkdirAll(path.Dir(genesisConfPath), 0755); err != nil {
+			panic(err)
+		}
+		if err := ioutil.WriteFile(genesisConfPath, util.StringifyJsonToBytes(ge), 0644); err != nil {
+			panic(err)
+		}
+
 		// 生成bootnode的key
 		b0Dir := filepath.Join(wDir, "b0")
+		if err := os.MkdirAll(b0Dir, 0755); err != nil {
+			panic(err)
+		}
 		nodeKey, _ := crypto.GenerateKey()
-		if err := crypto.SaveECDSA(filepath.Join(b0Dir, "boot.key"), nodeKey); err != nil {
+		b0keyfile := filepath.Join(b0Dir, "boot.key")
+		if err := crypto.SaveECDSA(b0keyfile, nodeKey); err != nil {
 			panic(err)
 		}
 
 		m0DataDir := filepath.Join(wDir, "m0")
+		if err := os.MkdirAll(m0DataDir, 0755); err != nil {
+			panic(err)
+		}
 
+		// 放入 bootnode key
+		udpPort, _ := strconv.ParseInt((":30301")[1:], 10, 64)
+		n := enode.NewV4(&nodeKey.PublicKey, net.ParseIP("127.0.0.1"), int(udpPort), int(udpPort))
+		bnconfB := util.StringifyJsonToBytes([]string{n.String()})
 
+		staticBFileName := "static_boot_nodes.json"
+		if err := ioutil.WriteFile(filepath.Join(m0DataDir, staticBFileName), bnconfB, 0644); err != nil {
+			panic(err)
+		}
+		for _, vd := range vDataDirs {
+			if err := ioutil.WriteFile(filepath.Join(vd, staticBFileName), bnconfB, 0644); err != nil {
+				panic(err)
+			}
+		}
 
+		// 创建启动脚本，bootnode、verifiers、minermaster
+		startsh := fmt.Sprintf(`
+export boots_env=local
 
+nohup bootnode --nodekey %v >> %v & 
+
+nohup dipperin --node_type 2 --soft_wallet_pwd 123 --data_dir %v --http_port 10001 --ws_port 10002 --p2p_listener 20001 >> %v &
+nohup dipperin --node_type 2 --soft_wallet_pwd 123 --data_dir %v --http_port 10003 --ws_port 10004 --p2p_listener 20002 >> %v &
+nohup dipperin --node_type 2 --soft_wallet_pwd 123 --data_dir %v --http_port 10005 --ws_port 10006 --p2p_listener 20003 >> %v &
+nohup dipperin --node_type 2 --soft_wallet_pwd 123 --data_dir %v --http_port 10007 --ws_port 10008 --p2p_listener 20004 >> %v &
+
+nohup dipperin --node_type 1 --soft_wallet_pwd 123 --data_dir %v --http_port 10010 --ws_port 10011 --p2p_listener 20010 >> %v &
+`, b0keyfile, filepath.Join(b0Dir, "out.log"),
+			vDataDirs[0], filepath.Join(vDataDirs[0], "out.log"),
+			vDataDirs[1], filepath.Join(vDataDirs[1], "out.log"),
+			vDataDirs[2], filepath.Join(vDataDirs[2], "out.log"),
+			vDataDirs[3], filepath.Join(vDataDirs[3], "out.log"),
+				m0DataDir, filepath.Join(m0DataDir, "out.log"))
+
+		if err := ioutil.WriteFile(filepath.Join(wDir, "start_nodes.sh"), []byte(startsh), 0744); err != nil {
+			panic(err)
+		}
 	}
 	app.Run(os.Args)
 }
